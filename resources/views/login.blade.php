ab79f606001a4ecaea67b23a784d1560e0d5e5e3
Davidle96 <171213490+Davidle96@users.noreply.github.com>
2025-01-06 23:29:12 +0700

    make form login ver 0.1

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -8,10 +8,15 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/js/bootstrap.bundle.min.js"></script>
     <title>Document</title>
 </head>
-<body>
-    <div>
+<body style="background-color: black; color: white;">
+    <div class="container">
         <form action="">
-            
+            <label for="" class="form-label">sdádsad</label>
+            <input type="text" class="form-control" placeholder="Username">
+            <label for="" class="form-label">dfsdfsdfd</label>
+            <input type="password" class="form-control" placeholder="Password">
+            <button type="submit" class="btn btn-primary">Login</button>
+            <button type="reset">Reset</button>
         </form>
     </div>
 </body>
