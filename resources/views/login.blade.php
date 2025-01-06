8a3e58863a8b892e6f85ff09529bbe91c457a130
Davidle96 <171213490+Davidle96@users.noreply.github.com>
2025-01-06 22:17:36 +0700

    ver 0.1

---
+++ resources/views/login.blade.php
@@ -0,0 +1,19 @@
+<!-- filepath: /f:/Program_Files/wamp/www/my_project/web_comic_detiue/resources/views/welcome.blade.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/js/bootstrap.bundle.min.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <form action="">
+            
+        </form>
+    </div>
+</body>
+</html>
